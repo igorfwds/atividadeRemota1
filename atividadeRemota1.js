0f4e3893d471bb8aa0db853f743f38165996d118
igorfwds <igorfwds@hotmail.com>
2022-06-10 16:44:31 -0300

    Etapa 3 concluída, forcei a mensagem de erro

--- atividadeRemota1.js
+++ atividadeRemota1.js
@@ -6,9 +6,9 @@ if ( pecaPeso >= 100) {
 } else {
     console.log("Ah não, infelizmente sua peça possui um peso de " + pecaPeso + " gramas, logo, você não poderá realizar o cadastro.");
 }
-
+console.log("");
 // • Dada a capacidade de cada caixa, caso a lista de peças seja superior a 10, imprima uma mensagem informando não ter capacidade suficiente.
-let listaDePecas = ["Cilindros", "bombas d'água", "coxins", "polias", "juntas homocinéticas", "semi eixos", "suspensão","Amortecedor da suspensão", "Anel de pistão", "Bomba elétrica"]
+let listaDePecas = ["Cilindro", "bombas d'água", "coxin", "polia", "JH", "semi eixo", "suspensão","Amortecedor da suspensão", "Anel de pistão", "Bomba elétrica"]
 let quantidadeDePecas = listaDePecas.length
 
 if (quantidadeDePecas > 10) {
@@ -16,6 +16,16 @@ if (quantidadeDePecas > 10) {
 } else {
     console.log("Uhul! seus itens logo serão empacotados!");
 }
+console.log("");
 
 // • Caso a peça tenha um nome com quantidade inferior a 3 caracteres, informe uma mensagem de erro.
 
+for (let inc = 0; inc <= listaDePecas.length-1; inc++ ){
+    let nomeDaPeca = listaDePecas[inc]
+    
+    if (nomeDaPeca.length < 3) {
+        console.log("ERRO! O item " + nomeDaPeca + " tem menos que 3 caracteres.");
+    }
+}
+console.log("");
+// PROVOQUEI A MENSAGEM DE ERRO, CHAMANDO A JUNTA HOMOCINÉTICA DE "JH".
